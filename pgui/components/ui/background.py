d0491d86e9708a50bb3028b9d6e458a44b4b9b89
Hua Nguyen Khoi <memuopmapdit1234@gmail.com>
2026-05-27 20:46:51 +0800

    refactor: fix pos for bg ui

--- pgui/components/ui/background.py
+++ pgui/components/ui/background.py
@@ -120,12 +120,18 @@ class BackgroundImage:
             self.__error_load_img = True
 
     @property
+    def size(self) -> Vec2:
+        return self.__background_img.get_size()
+    
+    @property
     def pos(self) -> Vec2:
         return self.__pos
     
     @pos.setter
-    def pos(self, value):
+    def pos(self, value: Vec2):
         self.__pos = value
+        if self.__error_load_img:
+            self.__background_img = pygame.Rect(self.__pos, self.__surface.get_size())
 
     @property
     def visible(self) -> bool:
@@ -141,7 +147,7 @@ class BackgroundImage:
                 pygame.draw.rect(self.__surface, hex_to_rbg("#000000"), self.__background_img)
             else: 
                 self.__background_img = pygame.transform.scale(self.__background_img, self.__surface.get_size())
-                self.__surface.blit(self.__background_img, (0,0))
+                self.__surface.blit(self.__background_img, self.__pos)
     
     
 
